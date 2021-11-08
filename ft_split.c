9902c77a9d67cfe668e5e9d11b218d35826e090a
M'hamed Ajjig <majjig@e1r7p10.1337.ma>
2021-11-08 15:55:49 +0100

    add free function

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: majjig <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 16:05:13 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/08 11:20:15 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/08 15:44:47 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,6 @@ static char	*ft_alloc_fill(char *dest, char *src, char c)
 	while (src[i] && src[i] != c)
 		i++;
 	dest = (char *) malloc((i + 1) * sizeof(char));
-	if (dest == NULL)
-	{
-		free(dest);
-		return(NULL);
-	}
 	i = 0;
 	while (src[i] && src[i] != c)
 	{
@@ -40,24 +35,34 @@ static int	ft_count_words(char *str, char c)
 	int	words;
 
 	words = 0;
-
 	while (*str)
 	{
 		if (*str != c)
-	  {
-		  words++;
-		while (*str && *str != c)
-			str++;
-		continue ;
-	  }
-	  str++;
+		{
+			words++;
+			while (*str && *str != c)
+				str++;
+			continue ;
+		}
+		str++;
 	}
 	return (words + 1);
 }
 
+int	ft_check_is_alloc(char **str, int j)
+{
+	if (str[j] == NULL)
+	{
+		while (j >= 0)
+			free(str[j--]);
+		free(str);
+		return (0);
+	}
+	return (1);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	int		i;
 	int		j;
 	int		words;
 	char	**str;
@@ -66,18 +71,19 @@ char	**ft_split(char const *s, char c)
 	str = (char **) malloc(words * sizeof(char *));
 	if (str == NULL || s == NULL)
 		return (NULL);
-	i = 0;
 	j = 0;
-	while (s[i])
+	while (*s)
 	{
-		if (s[i] != c)
+		if (*s != c)
 		{
-			str[j] = ft_alloc_fill(str[j], (char *) &s[i], c);
+			str[j] = ft_alloc_fill(str[j], (char *) s, c);
+			if (!(ft_check_is_alloc(str, j)))
+				return (NULL);
 			j++;
-			while (s[i + 1] && s[i + 1] != c)
-				i++;
+			while (*(s + 1) && *(s + 1) != c)
+				s++;
 		}
-		i++;
+		s++;
 	}
 	str[j] = NULL;
 	return (str);
