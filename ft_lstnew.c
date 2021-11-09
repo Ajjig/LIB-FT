f7c10215436b50b98455c5f35aab75a381df1a27
M'hamed Ajjig <majjig@e1r2p5.1337.ma>
2021-11-09 11:38:44 +0100

    protected ft_lstnew function from malloc NULL return

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -17,6 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*new;
 
 	new = malloc(sizeof(t_list *));
+	if (new == NULL)
+		return (NULL);
 	new->content = content;
 	new->next = NULL;
 	return (new);
