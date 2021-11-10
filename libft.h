51672150895319aa0af3255aef80504eebacc03f
M'hamed Ajjig <majjig@e2r2p9.1337.ma>
2021-11-10 15:06:46 +0100

    ft_lstdelone is DONE

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 17:37:05 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/09 20:47:55 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/10 15:05:58 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,7 @@ void		ft_lstadd_front(t_list **lst, t_list *new);
 int			ft_lstsize(t_list *lst);
 t_list		*ft_lstlast(t_list *lst);
 void		ft_lstadd_back(t_list **lst, t_list *new);
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
+void		ft_lstclear(t_list **lst, void (*del)(void *));
 
 #endif
