1a85bee1adddb2f31ce36dc5376a538599007a93
M'hamed Ajjig <majjig@e2r2p9.1337.ma>
2021-11-10 14:56:38 +0100

    ft_lstadd_back is DONE

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 20:48:28 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/09 20:51:30 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/10 14:49:07 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		while ((*lst)->next)
+			*lst = (*lst)->next;
+		(*lst)->next = new;
+	}
 }
