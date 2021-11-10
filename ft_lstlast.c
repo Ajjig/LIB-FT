b80fe1ee8ec3082e19593c96a6fdcf585c7754f7
M'hamed Ajjig <majjig@e2r2p9.1337.ma>
2021-11-10 14:55:54 +0100

    ft_lstlast is DONE

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 20:42:29 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/09 20:49:58 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/10 14:55:01 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	while(lst)
-	{
+	if (lst == NULL)
+		return (NULL);
+	while (lst->next)
 		lst = lst->next;
-	}
 	return (lst);
 }
