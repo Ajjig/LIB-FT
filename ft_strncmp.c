654890a84c8eaa7b5f9aacfef635d5213d0ea7b1
M'hamed Ajjig <majjig@e2r8p11.1337.ma>
2021-11-11 17:34:03 +0100

    added casting to unsigned char

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 16:08:29 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/10 22:16:30 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/11 16:45:42 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	return ((unsigned char) s2[i] - (unsigned char) s2[i]);
+	return (0);
 }
