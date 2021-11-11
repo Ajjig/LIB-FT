bd00c00232e616a0ad86f6613c3fd34c517b71a7
M'hamed Ajjig <majjig@e2r8p11.1337.ma>
2021-11-11 18:04:06 +0100

    use char casting instead of unsigned char

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: majjig <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 16:01:32 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/07 16:42:22 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/11 18:03:07 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char	*dst;
+	char	*dst;
 
 	if (dest == NULL && src == NULL)
 		return (NULL);
-	dst = (unsigned char *) dest;
+	dst = (char *) dest;
 	while (n--)
-		*dst++ = *(unsigned char *)src++;
+		*dst++ = *(char *)src++;
 	return (dest);
 }
