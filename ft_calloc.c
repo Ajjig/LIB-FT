14b30744a354e8a97afb0b84b1861187f93dba63
M'hamed Ajjig <majjig@e2r2p9.1337.ma>
2021-11-11 19:27:19 +0100

    ft_caloc is more readable now

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: majjig <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 15:48:37 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/07 15:55:55 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/11 19:24:47 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	*ft_calloc(size_t count, size_t size)
 
 	if (!count || !size)
 	{
-		count++;
-		size++;
+		count = 1;
+		size = 1;
 	}
 	ptr = malloc(count * size);
 	if (ptr == NULL)
